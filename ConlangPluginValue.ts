bdc4ffbe0a8ddb826cf9cc0550dc9781b5e756b4
Jacob Gantt <contact@jgantts.com>
2023-08-06 21:35:46 -0400

    effects?

--- ConlangPluginValue.ts
+++ ConlangPluginValue.ts
@@ -29,10 +29,15 @@ class ConlangPlugin implements PluginValue {
   }
 
   buildDecorations(view: EditorView): DecorationSet {
-    const builder = new RangeSetBuilder<Decoration>();
-    //const decorations: Range<Decoration>[] = [];
+    const decorations: Range<Decoration>[] = [];
+    decorations.push(Decoration.mark({class: "myconlang"}).range(0, 8))
+    return Decoration.set(decorations, true);
 
-    console.log("hflisl;l")
+    //const builder = new RangeSetBuilder<Decoration>();
+    //builder.add(0, 7, Decoration.mark({class: "myconlang"}))
+    //return builder.finish();
+  }
+  /*
 
     // For every valid parser ⟨ ⟩ pair (starting at 'start' and stopping at 'end', adda  decoration)
 
@@ -40,25 +45,24 @@ class ConlangPlugin implements PluginValue {
       syntaxTree(view.state).iterate({
         from, to,
         enter: (node) => {
-          //////////////////////////////////////////////
-          //This currently only finds the first result//
-          //////////////////////////////////////////////
           let current = view.state.doc.sliceString(node.from, node.to)
           let first = current.indexOf("⟨");
           let second = current.indexOf("⟩");
           console.log(`from: ${node.from} to: ${node.to} current: ${current} one: ${first} two: ${second} contents: ${current.substring(first, second+1)}`)
           if (first>=0 && second>=0) {
-            console.log(`range: ${node.from + first} thru ${node.from + second}`)
-            //builder.add(node.from + first, node.from + second, Decoration.mark({class: "myconlang"}))
-            builder.add(node.from + first, node.from + second, Decoration.mark({class: "myconlang"}))
+            decorations.push(Decoration.mark({class: "myconlang"}).range(node.from + first, node.from + second))
           }
-          console.log("uriopweu")
         }
       })
     }
-  
-    return builder.finish();
-  }
+
+    console.log(decorations)
+    return Decoration.set(decorations, true);
+  }*/
 }
 
-export const conlangPlugin = ViewPlugin.fromClass(ConlangPlugin);
+const conlangPluginSpec: PluginSpec<ConlangPlugin> = {
+  decorations: (value: ConlangPlugin) => value.decorations,
+};
+
+export const conlangPlugin = ViewPlugin.fromClass(ConlangPlugin, conlangPluginSpec);
