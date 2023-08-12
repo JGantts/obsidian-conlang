d865383183a506f84a47c97a958a8cb24dd53aed
Jacob Gantt <contact@jgantts.com>
2023-08-12 19:45:19 -0400

    Whaaaat

--- checker.ts
+++ checker.ts
@@ -12,11 +12,13 @@ export default {
       foundStart: number,
       foundEnd: number
     ) => {
-      console.log(`foundFunction: ${foundStart} "${text.substring(foundStart, foundEnd+1)}" ${foundEnd}`)
-      console.log('AAA')
-      console.log(`langSettings.class: ${langSettings.class}`)
+      /*console.log(
+        `foundFunction: ${foundStart} `
+        +`${text.substring(foundStart-2, foundStart)}`
+        +`_${text.substring(foundStart, foundEnd+1)}_`
+        +`${text.substring(foundEnd+1, foundEnd+3)}`
+        +` ${foundEnd}`)*/
       foundFunctionIn(foundStart, foundEnd)
-      console.log('BBB')
     }
     let maybeFoundFunction = (
       maybeFoundStart: number|null,
@@ -30,19 +32,11 @@ export default {
         foundFunction(final_orNull, final_orNull)
       }
     }
-    console.log(langSettings)
-    console.log(text)
     let initial_orNull = getNextXAfter_orNull(text, langSettings.open, -1)
     let final_orNull = getNextXAfter_orNull(text, langSettings.close, initial_orNull)
     let final_orEnd = getNextXAfter_orEnd(text, langSettings.close, initial_orNull)
-    console.log(
-      `initial_orNull: ${initial_orNull}\n`
-      +`final_orNull: ${final_orNull}\n`
-      +`final_orEnd: ${final_orEnd}\n`
-    )
     maybeFoundFunction(initial_orNull, final_orNull)
     while (initial_orNull != null) {
-      console.log(`basic initial-nextLinebreak_orEnd: ${initial_orNull} ${text.substring(initial_orNull, (final_orNull??0)+1)} ${final_orNull}`)
       const nextInitial_orNull = getNextXAfter_orNull(text, langSettings.open, initial_orNull!)
       const nextInitial_orEnd = getNextXAfter_orEnd(text, langSettings.open, initial_orNull!)
       const nextInitialFound = nextInitial_orNull!=null
@@ -65,10 +59,6 @@ export default {
         final_orNull = getNextXAfter_orNull(text, langSettings.close, initial_orNull)
         final_orEnd = getNextXAfter_orEnd(text, langSettings.close, initial_orNull)
       }
-      console.log(`nextInitialFound: ${nextInitialFound}`)
-      console.log(`nextLinebreak_orNull: ${nextLinebreak_orNull}`)
-      console.log(`nextInitial_orNull: ${nextInitial_orNull}`)
-      console.log(`doubleInitial: ${doubleInitial}`)
       const initialAtStart = 
         initial_orNull! == 0
         || text.substring(
@@ -82,9 +72,16 @@ export default {
       let aFinalFound = final_orNull!=null
       const isBroken =
         aFinalFound
-        && text.substring(initial_orNull!, final_orNull!).contains(linebreak);      
-      if (initialAtStart && (isBroken || !aFinalFound)) {
-        foundFunction(initial_orNull!, nextLinebreak_orEnd)
+        && existsXInBewtween(text, linebreak, initial_orNull!, final_orNull!)
+      const isOpenEnded = initialAtStart && (isBroken || !aFinalFound)
+      if (isOpenEnded) {
+        openEnded(
+          text,
+          langSettings,
+          linebreak,
+          initial_orNull!,
+          foundFunction
+        )
       } else if (initial_orNull!=null && final_orNull!=null && !isBroken) {
         foundFunction(initial_orNull!, final_orNull)
       } else if (isBroken) {
@@ -102,6 +99,122 @@ export default {
       final_orEnd = getNextXAfter_orEnd(text, langSettings.close, initial_orNull)
     }
     maybeFoundFunction(initial_orNull, final_orNull)
+  }
+}
+
+function openEnded(
+  text: string,
+  langSettings: { open: string; close: string; class: string; },
+  linebreak: string,
+  initial: number,
+  foundFunction: (
+    foundStart: number,
+    foundEnd: number
+  ) => void
+) {
+  const nextLinebreak_orEnd = getNextXAfter_orEnd(text, linebreak, initial)
+  let isCompleteClosedEnded: boolean
+  let potentialFinal = getNextXAfter_orNull(text, langSettings.close, initial)
+  if (potentialFinal!=null) {
+    const potentialFinalIsEnd = potentialFinal==text.length-langSettings.close.length
+    if (!potentialFinalIsEnd) {
+      let potentialSubsequentLineBreak =
+        getNextXAfter_orNull(text, linebreak, potentialFinal!)
+      const potentialFinalIsAtEndOfParagraph = 
+        ((potentialFinal!)+langSettings.close.length)
+        == potentialSubsequentLineBreak
+      if (!potentialFinalIsAtEndOfParagraph) {
+        potentialFinal = null
+      }
+    }
+  }
+  if (potentialFinal==null) {
+    isCompleteClosedEnded = false
+  } else {
+    let currentNewline: number|null = initial
+    let isDirty = false
+    while (currentNewline!=null && !isDirty) {
+      let lastNewlineInChainPrev: number = currentNewline
+      let done = false
+      while (!done) {
+        if (
+          text.substring(
+            lastNewlineInChainPrev,
+            lastNewlineInChainPrev+linebreak.length
+          )==linebreak
+        ) {
+          lastNewlineInChainPrev += linebreak.length
+        } else {
+          done = true
+        }
+      }
+      lastNewlineInChainPrev -= linebreak.length
+      currentNewline = getNextXAfter_orEnd(text, linebreak, lastNewlineInChainPrev)
+      let insideFinal = (currentNewline??0) <= potentialFinal+linebreak.length
+      let atFinal = (currentNewline??0) == potentialFinal+linebreak.length
+      if (!insideFinal) {
+        currentNewline = null
+      }
+      if (
+        currentNewline != null 
+        && !isDirty
+      ) {
+        const thisOpen_start = lastNewlineInChainPrev + linebreak.length
+        const thisOpen_end = thisOpen_start + langSettings.open.length
+        const thisClose_end = currentNewline!
+        const thisClose_start = thisClose_end - langSettings.close.length
+
+        const thisLine_start = thisOpen_start
+        const thisLineExclOpen_start = thisOpen_end
+        const thisLine_end = thisClose_end
+        const thisLineExclClose_end = thisClose_start
+
+        //dirty if first char is not open
+        isDirty = isDirty
+          || text.substring(
+            thisOpen_start,
+            thisOpen_end
+          ) != langSettings.open
+
+        //dirty if any char except first is open
+        isDirty = isDirty
+          || existsXInBewtween(
+            text,
+            langSettings.open,
+            thisLineExclOpen_start,
+            thisLine_end
+          )
+
+        if (!atFinal) {
+          //dirty if any char close
+          isDirty = isDirty
+            || existsXInBewtween(
+              text,
+              langSettings.close,
+              thisLine_start,
+              thisLine_end
+            )
+        } else {
+          //dirty if last char not close
+          isDirty = isDirty
+          || text.substring(
+            thisClose_start,
+            thisClose_end
+          ) != langSettings.close
+        }
+      }
+    }
+    if (isDirty) {
+      //console.log("wut "+text.substring(lastNewlineInChainPrev+linebreak.length+1, currentNewline))
+      isCompleteClosedEnded = false
+    } else {
+      isCompleteClosedEnded = true
+    }
+  }
+  if (isCompleteClosedEnded) {
+    foundFunction(initial, nextLinebreak_orEnd)
+  } else {
+    foundFunction(initial, initial)
   }
 }
 
@@ -133,4 +246,13 @@ function getNextXAfter_orEnd(
       ? text.length-1
       : nextAfter ?? 0
   return toReturn
+}
+
+function existsXInBewtween(
+  text: string,
+  x_string: string,
+  initial: number,
+  final: number,
+) {
+  return text.substring(initial!, final!).contains(x_string); 
 }
